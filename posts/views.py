f71f9cc981b8a0f3e431865fb94f2387fb9dfee6
donguk0207 <donguk0207@gmail.com>
2025-02-20 17:12:15 +0900

    #1. django test

---
+++ posts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def feeds(request):
+    if not request.user.is_authenticated:
+        return redirect("/users/login/")
+    return render(request, "posts/feeds.html")
+
+#    user = request.user
+#    is_authenticated = user.is_authenticated
+
